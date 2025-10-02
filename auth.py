fb0a7fc11cf656af1fdcbfaf8a21b18414bdc04f
Cauã Freitas <cauafreitas026@gmail.com>
2025-10-02 13:29:53 -0300

    V 3.1.1

--- auth.py
+++ auth.py
@@ -190,17 +190,16 @@ def show_login_form():
     # Bloco principal que contém a logo e o formulário
     st.markdown('<div class="login-block">', unsafe_allow_html=True)
     
-    st.markdown("""
-        <div class="login-logo-text">
-            <span class="login-logo-asset">ASSET</span><span class="login-logo-flow">FLOW</span>
-        </div>
-    """, unsafe_allow_html=True)
-
     if 'show_reset_form' not in st.session_state:
         st.session_state.show_reset_form = False
 
     if st.session_state.show_reset_form:
         with st.form("form_reset_request"):
+            st.markdown("""
+                <div class="login-logo-text">
+                    <span class="login-logo-asset">ASSET</span><span class="login-logo-flow">FLOW</span>
+                </div>
+            """, unsafe_allow_html=True)
             st.markdown('<h1 class="card-title">Redefinir Senha</h1>', unsafe_allow_html=True)
             st.markdown('<p class="form-label" style="text-align: left;">Seu login (e-mail)</p>', unsafe_allow_html=True)
             login_para_reset = st.text_input("Seu login (e-mail)", key="reset_email_input", label_visibility="collapsed")
@@ -263,3 +262,4 @@ def logout():
     for key in ['user_login', 'user_role', 'user_name', 'user_id']:
         st.session_state.pop(key, None)
     st.rerun()
+
